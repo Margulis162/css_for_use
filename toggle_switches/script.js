37ed8f24c3fe731cf6a355f871b5be0644b557f0
Margulis162 <markmargulis@proton.me>
2024-03-18 09:30:44 -0400

    I've started to work on the next switch
    It's  going to be awesome! Most likely, there is a chance. I think it will be a zero jumping in between square brackets. What effects and/or animations do I want to add to it?

--- toggle_switches/script.js
+++ toggle_switches/script.js
@@ -1,3 +1,6 @@
 const switch1 = document.getElementById('switch-1')
+const switch2 = document.getElementById('switch-2')
 
 window.setTimeout(() => switch1.style.visibility = 'visible', 1000)
+
+document.addEventListener('change',() => console.log(switch2.control.checked))

005a585fbc82ba922966b6d4101979eee96f5e2c
Margulis162 <markmargulis@proton.me>
2024-03-19 09:31:11 -0400

    Switch 2 design advancement
    Inner text gets changed to 1 from 0 if checked is true now. I also played with css a bit. The struggle is I want to keep it as much  ad plain text as possible, but I also want it to be obviously clickable. Have to think about it for a bit.

--- toggle_switches/script.js
+++ toggle_switches/script.js
@@ -3,4 +3,10 @@ const switch2 = document.getElementById('switch-2')
 
 window.setTimeout(() => switch1.style.visibility = 'visible', 1000)
 
-document.addEventListener('change',() => console.log(switch2.control.checked))
+switch2.addEventListener('change',() => {
+    if(switch2.childNodes[3].textContent == '0'){
+        switch2.childNodes[3].textContent = '1'}
+    else{
+        switch2.childNodes[3].textContent = '0'
+    }
+})

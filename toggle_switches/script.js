aded50c59efa841bdce477b53ed3c89d3586f84e
Margulis162 <markmargulis@proton.me>
2024-03-21 14:58:18 -0400

    I have developed a style I'm okay with.
    Square brackets are replaced with borders, and '1' indicating that switch is on replaced with 'c'. I want to add a glitch effect on hover to it. But I'm on very early stage of it's implementation.

--- toggle_switches/script.js
+++ toggle_switches/script.js
@@ -3,10 +3,17 @@ const switch2 = document.getElementById('switch-2')
 
 window.setTimeout(() => switch1.style.visibility = 'visible', 1000)
 
+//not sure if I like it  switch 2 changing text content on check on
 switch2.addEventListener('change',() => {
-    if(switch2.childNodes[3].textContent == '0'){
-        switch2.childNodes[3].textContent = '1'}
+    if(switch2.childNodes[5].textContent == 'o'){
+        switch2.childNodes[5].textContent = 'c'}
     else{
-        switch2.childNodes[3].textContent = '0'
+        switch2.childNodes[5].textContent = 'o'
     }
+})
+
+switch2.addEventListener('mouseover', (event)=>{
+    event.stopPropagation()
+    console.log(event.target)
+    setInterval(()=> event.target.childNodes[1].src  = 'toggle_switches/assets/glitch-2.svg', 1500)
 })
